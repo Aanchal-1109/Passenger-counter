91bf1eacfd2f8b15b164fc06d7a079ee99d183ef
Aanchal laad <aanchallaad2002@gmail.com>
2022-08-17 23:17:47 +0530

    Passenger-counter

---
+++ index.js
@@ -0,0 +1,21 @@
+let saveElement = document.getElementById("save-element")
+let countElement = document.getElementById("count-element")
+let count = 0
+
+function increment() {
+    count += 1
+    countElement.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveElement.textContent += countStr
+    countElement.textContent = 0
+    count = 0
+}
+
+
+
+
+
+
